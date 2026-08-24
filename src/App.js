f850990249941a13431e9b3f19f8feca57a17b0f
Ibraheem Butt <dashingibraheem@gmail.com>
2026-08-25 02:11:59 +0500

    Final-Push Here We Go Live

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className="app">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <ScrollToTop/>
         <Switch>
           <Route path="/" exact component={Main} />
