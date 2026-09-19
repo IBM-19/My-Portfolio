e06f1f53aa72d28001dd91b57d6f3333d148ea73
Ibraheem Butt <dashingibraheem@gmail.com>
2026-09-20 04:43:32 +0500

    SEO updates and build fixes

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -8,7 +8,7 @@ function Main() {
     return (
         <div>
             <Helmet>
-                <title>{headerData.name} - Porfolio</title>
+                <title>{headerData.name} — AI Student & Graphic Designer | ITU Lahore</title>
             </Helmet>
 
             <Navbar />
